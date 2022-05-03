bac11cf13fa30205455fee99fc50f6c3d7ed47b7
Marina <m.pozhidayeva@gmail.com>
2022-05-03 13:07:06 +0200

    change_sol

--- matching_brackets.py
+++ matching_brackets.py
@@ -1,5 +1,5 @@
 #Dictionaries, Lists, Conditionals
-# 1. Create Stack
+
 def is_paired(input_string):
     d = dict(zip(['[', '{', '('], [']','}',')']))
     s = []
@@ -10,18 +10,11 @@ def is_paired(input_string):
         elif symb in d.values():
             if not s:
                 return False
-                
-            current_char = s.pop() #pick and return the last char in the stack
-            if current_char == '(':
-                if symb != ")":
-                    return False
-            if current_char == '{':
-                if symb != "}":
-                    return False
-            if current_char == '[':
-                if symb != "]":
-                    return False
 
+        if s[-1] == d[symb]:
+            s.pop()
+        else:
+            return False
 
     return len(s) == 0
         
