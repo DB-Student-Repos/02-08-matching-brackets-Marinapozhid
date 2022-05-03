da2e1a4b0f539d685a0f5c3c3e5125875bd03813
Marina <m.pozhidayeva@gmail.com>
2022-05-03 12:13:15 +0200

    matching_brackets_solution

--- matching_brackets.py
+++ matching_brackets.py
@@ -1,2 +1,29 @@
+#Dictionaries, Lists, Conditionals
+# 1. Create Stack
 def is_paired(input_string):
-    pass
+    d = dict(zip(['[', '{', '('], [']','}',')']))
+    s = []
+    for symb in input_string:
+        if symb in d.keys():
+           s.append(symb)
+
+        elif symb in d.values():
+            if not s:
+                return False
+                
+            current_char = s.pop() #pick and return the last char in the stack
+            if current_char == '(':
+                if symb != ")":
+                    return False
+            if current_char == '{':
+                if symb != "}":
+                    return False
+            if current_char == '[':
+                if symb != "]":
+                    return False
+
+
+    return len(s) == 0
+        
+
+
