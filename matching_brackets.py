bfc56a844c221738078e01c6886e357cadf24a6a
Marina <m.pozhidayeva@gmail.com>
2022-05-05 10:17:21 +0200

    hm

--- matching_brackets.py
+++ matching_brackets.py
@@ -1,22 +1,21 @@
 #Dictionaries, Lists, Conditionals
 
 def is_paired(input_string):
-    d = dict(zip(['[', '{', '('], [']','}',')']))
+    d = dict(zip('[{(', ']})'))
     s = []
     for symb in input_string:
         if symb in d.keys():
            s.append(symb)
-
-        elif symb in d.values():
+        if symb in d.values():
             if not s:
                 return False
 
-        if s[-1] == d[symb]:
+        if s[-1] == d(symb):
             s.pop()
         else:
             return False
 
-    return len(s) == 0
+    return not s
         
 
 
